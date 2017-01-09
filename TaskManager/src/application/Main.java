e168d5ddb26aa6af89efb6183d6b2de58f9cf8e5
e152234 <e152234@E-0582.univ-lemans.fr>
2017-01-09 15:45:32 +0100

    FXML

--- TaskManager/src/application/Main.java
+++ TaskManager/src/application/Main.java
@@ -1,26 +1,32 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+
+		try { 
+			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("../TaskManager.fxml"));
+			Scene scene = new Scene(page);
+			scene.setRoot(page);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("TaskManager");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+
+public static void main(String[] args) {
+	launch(args);
+}
 }
