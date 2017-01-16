834e2e4c06c29b8a7eb9c9b3b3bbeb6387ac4d73
i131107 <i131107@E-0386.univ-lemans.fr>
2017-01-16 15:30:31 +0100

    Graphic

--- TaskManager/src/view/Main.java
+++ TaskManager/src/view/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 
 		try { 
-			AnchorPane page = (AnchorPane) FXMLLoader.load(Main.class.getResource("../view/TaskManager.fxml"));
+			GridPane page = (GridPane) FXMLLoader.load(Main.class.getResource("../view/TaskManager.fxml"));
 			Scene scene = new Scene(page);
 			scene.setRoot(page);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
