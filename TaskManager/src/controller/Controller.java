6485290248d87680228b88c8e15cdf7cd7fb6940
e152234 <e152234@E-0395.univ-lemans.fr>
2017-01-16 15:23:45 +0100

    Xml writer when sign up

--- TaskManager/src/controller/Controller.java
+++ TaskManager/src/controller/Controller.java
@@ -1,6 +1,11 @@
 package controller;
 
+import java.io.FileWriter;
 import java.io.IOException;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,33 +29,52 @@ public class Controller {
 	@FXML
 	private Text statusbar;
 
-	public void handleConnect(ActionEvent event){
-		if(username.getText().isEmpty())
+	public void handleConnect(ActionEvent event) {
+		if (username.getText().isEmpty())
 			statusbar.setText("Error username is empty");
-		else if(password.getText().isEmpty())
+		else if (password.getText().isEmpty())
 			statusbar.setText("Error password is empty");
 		else
 			statusbar.setText("Connected");
 
 	}
 
-	public void handleRegister(ActionEvent event){
-
+	public void handleRegister(ActionEvent event) {
 		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/Register.fxml"));
+			Parent root1 = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root1));
 
-	
-			 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/Register.fxml"));
-             Parent root1 = (Parent) fxmlLoader.load();
-             Stage stage = new Stage();
-             stage.setScene(new Scene(root1));
-   
-             stage.show();
-	
-		}
-		catch (IOException e) {
+			stage.show();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	public void handleSignUp(ActionEvent event) {
+		XMLOutputFactory factory = XMLOutputFactory.newInstance();
+
+		try {
+			XMLStreamWriter writer = factory.createXMLStreamWriter(new FileWriter("../../BD/users.xml"));
+
+			writer.writeStartDocument();
+			writer.writeStartElement("document");
+			writer.writeStartElement("data");
+			writer.writeAttribute("name", "value");
+			writer.writeEndElement();
+			writer.writeEndElement();
+			writer.writeEndDocument();
+
+			writer.flush();
+			writer.close();
+
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
