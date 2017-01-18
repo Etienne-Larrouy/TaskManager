20a07875b19c7818605138d61d90c45745de2d8b
i131107 <i131107@E-0461.univ-lemans.fr>
2017-01-18 15:56:40 +0100

    egz

--- TaskManager/src/view/Main.java
+++ TaskManager/src/view/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("TaskManager");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
