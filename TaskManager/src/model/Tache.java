612ddaea4b8d7140dc493112d940b50e7ac60f0b
e152234 <e152234@E-0624.univ-lemans.fr>
2017-01-19 11:34:02 +0100

    Model linked to task in FlowPanel

--- TaskManager/src/model/Tache.java
+++ TaskManager/src/model/Tache.java
@@ -1,6 +1,10 @@
 package model;
 
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -8,34 +12,60 @@ import javafx.beans.property.StringProperty;
 public class Tache {
 	private StringProperty title = new SimpleStringProperty();
 	private StringProperty description = new SimpleStringProperty();
-	
-	
+	private StringProperty state = new SimpleStringProperty();
+	private StringProperty deadLine = new SimpleStringProperty();
 	private int priorite;
-	private int etat;
+
 	private Utilisateur owner;
 	private Utilisateur performer;
-	
 
-	
-	public Tache(String title, String description){
+	public Tache(String title, String description, Utilisateur owner, Utilisateur performer, String state,
+			String deadLine) {
 		this.setDescription(description);
 		this.setTitle(title);
+		this.setState(state);
+		this.setDeadLine(deadLine);
+
+		this.owner = owner;
+		this.performer = performer;
+
 	}
-	
-	/* Getters and setters*/
+
+	/* Getters and setters */
+
+	// Owner
+	public Utilisateur getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Utilisateur owner) {
+		this.owner = owner;
+	}
+
+	// Performer
+	public Utilisateur getPerformer() {
+		return performer;
+	}
+
+	public void setPerformer(Utilisateur performer) {
+		this.performer = performer;
+	}
+
+	// Title
 	public final String getTitle() {
 		return this.title.get();
 	}
 
-	public void setTitle(String title) {
+	public final void setTitle(String title) {
 		this.title.set(title);
 	}
 
-	public StringProperty getTitleProperty() {
+	public final StringProperty getTitleProperty() {
 		return this.title;
 	}
-	
-	public StringProperty getDescriptionProperty() {
+
+	// Description
+	public final StringProperty getDescriptionProperty() {
 		return this.description;
 	}
 
@@ -43,9 +73,34 @@ public class Tache {
 		return this.description.get();
 	}
 
+	public final void setDescription(String description) {
+		this.description.set(description);
+	}
 
-	public void setDescription(String texte) {
-		this.description.set(texte);
+	// State
+	public final void setState(String state) {
+		this.state.set(state);
+	}
+
+	public final StringProperty getStateProperty() {
+		return this.state;
+	}
+
+	public final String getState() {
+		return this.state.get();
+	}
+
+	// DeadLine
+	public final void setDeadLine(String deadLine) {
+		this.deadLine.set(deadLine);
+	}
+
+	public final StringProperty getDeadLineProperty() {
+		return this.deadLine;
+	}
+
+	public final String getDeadline() {
+		return this.deadLine.get();
 	}
 
 }
--- TaskManager/src/model/Utilisateur.java
+++ TaskManager/src/model/Utilisateur.java
@@ -1,56 +1,26 @@
 package model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Utilisateur
 {
 	private int id;
 	private ArrayList<Tache> lTache = new ArrayList<Tache>();
-    private static int    _port;
-    private static Socket _socket;
+	private StringProperty username = new SimpleStringProperty(); 
+	
+	public Utilisateur(String username){
+		this.username.set(username);
+	}
+	/* Getters and setters*/
+	public final String getUsername() {
+		return this.username.get();
+	}
 
-    public static void main(String[] args)
-    {
-        String message      = "ping";
-        InputStream input   = null;
-   
-        try
-        {
-            _port   = (args.length == 1) ? Integer.parseInt(args[0]) : 8080;
-            _socket = new Socket((String) null, _port);
+	public StringProperty getUsernameProperty() {
+		return this.username;
+	}
 
-            // Open stream
-            input = _socket.getInputStream();
-
-            // Show the server response
-            String response = new BufferedReader(new InputStreamReader(input)).readLine();
-            System.out.println("Server message: " + response);
-        }
-        catch (UnknownHostException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            try
-            {
-                input.close();
-                _socket.close();
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
 }
